a00cbc3c79d38dc8970dc4df7a133f39d22b1c94
drevil <drevil@tilde.club>
2024-04-15 23:38:08 -0400

    added absolute wheel support for xp pen 15.6 pro

--- OpenTabletDriver.Configurations/Parsers/XP_Pen/XP_PenAuxReport.cs
+++ OpenTabletDriver.Configurations/Parsers/XP_Pen/XP_PenAuxReport.cs
@@ -2,8 +2,10 @@ using OpenTabletDriver.Tablet;
 
 namespace OpenTabletDriver.Configurations.Parsers.XP_Pen
 {
-    public struct XP_PenAuxReport : IAuxReport
+    public struct XP_PenAuxReport : IAuxReport, IAbsoluteWheelReport
     {
+        private const int WheelSteps = 90;
+        private static int WheelPositionCounter = 0;
         public XP_PenAuxReport(byte[] report, int index = 2)
         {
             Raw = report;
@@ -21,9 +23,30 @@ namespace OpenTabletDriver.Configurations.Parsers.XP_Pen
                 report[index + 1].IsBitSet(0),
                 report[index + 1].IsBitSet(1),
             };
+
+
+            var wheelData = report[7];
+
+                if(wheelData == 2)
+                {
+                    WheelPositionCounter += 1;
+                }
+                else if(wheelData == 1)
+                {
+                    WheelPositionCounter -= 1;
+                }
+
+                if(WheelPositionCounter < 0)
+                {
+                    WheelPositionCounter = WheelSteps - 1;
+                }
+                WheelPositionCounter %= WheelSteps;
+                WheelPosition = (uint?)WheelPositionCounter;
+            }
         }
 
         public bool[] AuxButtons { set; get; }
         public byte[] Raw { set; get; }
+        public uint? WheelPosition { get; set; }
     }
 }
